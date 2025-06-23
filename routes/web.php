c642beefa2a9623ed7d0eeaa1abd68abe74a468b
Frugone <frugone@gmail.com>
2025-06-23 15:06:46 +0200

    first commit

---
+++ routes/web.php
@@ -0,0 +1,10 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::post('login', [AuthController::class, 'login']);
+
+Route::middleware('auth:api')->group(function () {
+    Route::get('me', [AuthController::class, 'me']);
+    Route::post('logout', [AuthController::class, 'logout']);
+});
